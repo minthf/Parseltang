9b385d1e863f399dad8ca07c50755e14032db80c
Mintimir <kachaev.666@gmail.com>
2020-04-07 22:19:43 +0600

    lab #01

---
+++ lab1/zonk_points.py
@@ -0,0 +1,59 @@
+def set_of_three_or_more(dice):  # функция проверяет комбинации: три одинаковых, четвертая/пятая/шестая кость
+	temp_sum = 0
+	points = {  # хранятся очки для комбинаций
+		1: 1000,
+		2: 200,
+		3: 300,
+		4: 400,
+		5: 500,
+		6: 600
+	}
+	for point, score in points.items():
+		if dice.count(point) >= 3:
+			multiplier = dice.count(point) - 2
+			temp_sum += score * multiplier
+			if point == 1 or point == 5:
+				global run_ones_fives
+				run_ones_fives = False
+	return temp_sum
+
+
+def ones_and_fives(dice, dice_sum):
+	for element in dice:
+		if element == 1:
+			dice_sum = dice_sum + 100
+		if element == 5:
+			dice_sum = dice_sum + 50
+	return dice_sum
+
+
+def all_different(dice):
+	if dice == [1, 2, 3, 4, 5, 6]:
+		return True
+
+
+def three_couple(dice):
+	count = 0
+	for i in range(1, 7):
+		if dice.count(i) == 2:
+			count += 1
+	if count == 3:
+		return True
+
+
+def calc_zonk_combo(dice):
+	global run_ones_fives  # переменная проверяет были ли пары из 3х
+	run_ones_fives = True
+	dice.sort()
+	dice_sum = 0
+	if all_different(dice):
+		return 1500
+	if three_couple(dice):
+		return 750
+	sum_of_set_of_three = set_of_three_or_more(dice)
+	dice_sum += sum_of_set_of_three
+	if run_ones_fives:
+		dice_sum = ones_and_fives(dice, dice_sum)
+	return dice_sum
+
+
---
+++ lab1/zonk_points_test.py
@@ -0,0 +1,68 @@
+import unittest
+from zonk_points import calc_zonk_combo
+
+
+class TestZonkPoints(unittest.TestCase):
+	def test_zonk_points_ones_and_fives(self):
+		self.assertEqual(calc_zonk_combo([1, 1, 5, 5, 2, 3]), 300)
+
+	def test_zonk_points_ones_and_fives_2(self):
+		self.assertEqual(calc_zonk_combo([1, 1, 3, 5, 2, 3]), 250)
+
+	def test_zonk_points_set_of_three(self):
+		self.assertEqual(calc_zonk_combo([2, 2, 2, 4, 6, 3]), 200)
+
+	def test_zonk_points_ones_and_fives_and_set_of_three(self):
+		self.assertEqual(calc_zonk_combo([2, 2, 2, 1, 5, 3]), 350)
+
+	def test_zonk_points_set_of_four(self):
+		self.assertEqual(calc_zonk_combo([2, 2, 2, 2, 4, 3]), 400)
+
+	def test_zonk_points_set_of_five(self):
+		self.assertEqual(calc_zonk_combo([3, 3, 3, 3, 4, 3]), 900)
+
+	def test_zonk_points_set_of_six(self):
+		self.assertEqual(calc_zonk_combo([6, 6, 6, 6, 6, 6]), 2400)
+
+	def test_zonk_points_double_set_of_three(self):
+		self.assertEqual(calc_zonk_combo([6, 6, 6, 3, 3, 3]), 900)
+
+	def test_zonk_points_set_of_three_and_ones(self):
+		self.assertEqual(calc_zonk_combo([6, 6, 6, 1, 1, 7]), 800)
+
+	def test_zonk_points_set_of_six_2(self):
+		self.assertEqual(calc_zonk_combo([1, 1, 1, 1, 1, 1]), 4000)
+
+	def test_zonk_points_six_different(self):
+		self.assertEqual(calc_zonk_combo([1, 2, 3, 4, 5, 6]), 1500)
+
+	def test_zonk_points_three_couple(self):
+		self.assertEqual(calc_zonk_combo([1, 1, 2, 2, 3, 3]), 750)
+
+	def test_zonk_points_all_different(self):
+		self.assertEqual(calc_zonk_combo([2, 4, 5, 3, 1, 6]), 1500)
+
+	def test_zonk_points_fourth_fives_sixth_dice(self):
+		self.assertEqual(calc_zonk_combo([6, 6, 6, 6, 6, 6]), 2400)
+
+	def test_zonk_points_one_couple(self):
+		self.assertEqual(calc_zonk_combo([1, 1, 1]), 1000)
+
+	def test_zonk_points_four_same_and_one_couple(self):
+		self.assertEqual(calc_zonk_combo([1, 1, 1, 1, 2, 2]), 2000)
+
+	def test_zonk_points_one(self):
+		self.assertEqual(calc_zonk_combo([1]), 100)
+
+	def test_zonk_points_five(self):
+		self.assertEqual(calc_zonk_combo([5]), 50)
+
+	def test_zonk_points_no_combo(self):
+		self.assertEqual(calc_zonk_combo([3, 4, 6, 7]), 0)
+
+	def test_zonk_points_empty_dice(self):
+		self.assertEqual(calc_zonk_combo([]), 0)
+
+
+if __name__ == '__main__':
+	unittest.main()
