3ac438c78d90427bc3d516e1955d11acddd3d15c
Mintimir <kachaev.666@gmail.com>
2020-04-09 02:30:52 +0600

    lab#02, zonk_console

--- lab2/zonk_console.py
+++ lab2/zonk_console.py
@@ -37,6 +37,17 @@ def circle(first: Point, second: Point, third: Point, fourth: Point, center: Poi
 		                       (center.y + ((second.y + third.y) / 2)) / 2, width=5)
 
 
+def from_degrees_to_radians(degree):
+	radian = degree * (pi / 180)
+	return radian
+
+
+def find_point(start: Point, angle, dice_length):
+	x = start.x + (cos(angle) * dice_length)
+	y = start.y - (sin(angle) * dice_length)
+	return Point(x, y)
+
+
 def create_dice():
 	place_dice.delete("all")
 	dice = []
@@ -44,24 +55,24 @@ def create_dice():
 		x = random.randint(50, 350)
 		y = random.randint(50, 150)
 		first_point = Point(x, y)
-		angle = random.randint(1, 360) * (3.1415926 / 180)
-		x2 = x + (cos(angle) * dice_length)
-		y2 = y - (sin(angle) * dice_length)
+		angle = from_degrees_to_radians(random.randint(1, 360))
+		x2 = first_point.x + (cos(angle) * dice_length)
+		y2 = first_point.y - (sin(angle) * dice_length)
 		second_point = Point(x2, y2)
-		x4 = x + (sin(angle) * dice_length)
-		y4 = y + (cos((360 * (3.1415926 / 180)) - angle) * dice_length)
+		x4 = first_point.x + (sin(angle) * dice_length)
+		y4 = first_point.y + (cos(from_degrees_to_radians(360) - angle) * dice_length)
 		fourth_point = Point(x4, y4)
-		ox = (x2 + x4) / 2
-		oy = (y2 + y4) / 2
+		ox = (second_point.x + fourth_point.x) / 2
+		oy = (second_point.y + fourth_point.y) / 2
 		center_point = Point(ox, oy)
-		x3 = 2 * ox - x
-		y3 = 2 * oy - y
+		x3 = 2 * center_point.x - first_point.x
+		y3 = 2 * center_point.y - first_point.y
 		third_point = Point(x3, y3)
 		place_dice.create_polygon(first_point.x, first_point.y, second_point.x, second_point.y, third_point.x,
 		                          third_point.y, fourth_point.x, fourth_point.y, fill='white')
 		circle(first_point, second_point, third_point, fourth_point, center_point, dice)
 	points = calc_zonk_combo(dice)
-	label_points = Label(text = points, font = "Arial 14")
+	label_points = Label(text=points, font="Arial 14")
 	label_points.place(x=200, y=250)
 
 
@@ -75,11 +86,8 @@ root.title("Zonk")
 place_dice = Canvas(root, width=400, height=200, bg='green')
 place_dice.place(x=10, y=10)
 
-
-
 button_play = Button(root, text="roll the dice", command=create_dice)
 button_quit = Button(root, text="Quit", command=quit)
-
 
 button_play.place(x=100, y=300)
 button_quit.place(x=300, y=300)
