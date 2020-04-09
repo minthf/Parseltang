da4562b27479fd8f6f162f65ea2ec1e49f3193cb
Mintimir <kachaev.666@gmail.com>
2020-04-09 23:41:53 +0600

    Syntax reformat

--- lab2/zonk_console.py
+++ lab2/zonk_console.py
@@ -3,7 +3,7 @@ from math import *
 import random
 from zonk_points import calc_zonk_combo
 
-dice_length = 40
+DICE_LENGTH = 40
 
 
 class Point:
@@ -18,9 +18,12 @@ class Dice:
 		self.second_point = second
 		self.third_point = third
 		self.fourth_point = fourth
+
 	def draw_dice(self):
-		place_dice.create_polygon(self.first_point.x, self.first_point.y, self.second_point.x, self.second_point.y, self.third_point.x,
-		                          self.third_point.y, self.fourth_point.x, self.fourth_point.y, fill='white')
+		place_dice.create_polygon(
+			self.first_point.x, self.first_point.y, self.second_point.x, self.second_point.y,
+		    self.third_point.x, self.third_point.y, self.fourth_point.x, self.fourth_point.y, fill='white'
+		)
 
 
 class Circle:
@@ -29,34 +32,35 @@ class Circle:
 		self.second_point = second
 
 	def draw_circle(self):
-		place_dice.create_oval((self.first_point.x + self.second_point.x) / 2,
-		                       (self.first_point.y + self.second_point.y) / 2,
-		                       (self.first_point.x + self.second_point.x) / 2,
-		                       (self.first_point.y + self.second_point.y) / 2, width=5)
+		place_dice.create_oval(
+			(self.first_point.x + self.second_point.x) / 2,
+			(self.first_point.y + self.second_point.y) / 2,
+			(self.first_point.x + self.second_point.x) / 2,
+			(self.first_point.y + self.second_point.y) / 2, width=5)
 
 
 def quantity_of_circles(first: Point, second: Point, third: Point, fourth: Point, center: Point, dice_points: list):
 	circle_quantity = random.randint(1, 6)
 	dice_points.append(circle_quantity)
-	if (circle_quantity == 1 or circle_quantity == 5 or circle_quantity == 3):
+	if circle_quantity in [1, 5, 3]:
 		first_circle = Circle(center, center)
 		first_circle.draw_circle()
-	if (
-			circle_quantity == 2 or circle_quantity == 3 or circle_quantity == 4 or circle_quantity == 5 or circle_quantity == 6):
+	if circle_quantity in [2, 3, 4, 5, 6]:
 		second_circle = Circle(center, first)
 		second_circle.draw_circle()
 
 		third_circle = Circle(center, third)
 		third_circle.draw_circle()
-	if (circle_quantity == 4 or circle_quantity == 6 or circle_quantity == 5):
+	if circle_quantity in [4, 5, 6]:
 		fourt_circle = Circle(center, fourth)
 		fourt_circle.draw_circle()
 
 		fifth_circle = Circle(center, second)
 		fifth_circle.draw_circle()
-	if (circle_quantity == 6):
+	if circle_quantity == 6:
 		point_for_sixth_circle = Point((first.x + fourth.x) / 2, (first.y + fourth.y) / 2)
 		point_for_seveth_circle = Point((second.x + third.x) / 2, (second.y + third.y) / 2)
+
 		sixth_circle = Circle(center, point_for_sixth_circle)
 		sixth_circle.draw_circle()
 
@@ -69,9 +73,6 @@ def from_degrees_to_radians(degree):
 	return radian
 
 
-
-
-
 def create_dice():
 	place_dice.delete("all")
 	dice_points = []
@@ -80,11 +81,11 @@ def create_dice():
 		y = random.randint(50, 150)
 		first_point = Point(x, y)
 		angle = from_degrees_to_radians(random.randint(1, 360))
-		x2 = first_point.x + (cos(angle) * dice_length)
-		y2 = first_point.y - (sin(angle) * dice_length)
+		x2 = first_point.x + (cos(angle) * DICE_LENGTH)
+		y2 = first_point.y - (sin(angle) * DICE_LENGTH)
 		second_point = Point(x2, y2)
-		x4 = first_point.x + (sin(angle) * dice_length)
-		y4 = first_point.y + (cos(from_degrees_to_radians(360) - angle) * dice_length)
+		x4 = first_point.x + (sin(angle) * DICE_LENGTH)
+		y4 = first_point.y + (cos(from_degrees_to_radians(360) - angle) * DICE_LENGTH)
 		fourth_point = Point(x4, y4)
 		ox = (second_point.x + fourth_point.x) / 2
 		oy = (second_point.y + fourth_point.y) / 2
@@ -92,7 +93,7 @@ def create_dice():
 		x3 = 2 * center_point.x - first_point.x
 		y3 = 2 * center_point.y - first_point.y
 		third_point = Point(x3, y3)
-		dice = Dice(first_point,second_point,third_point,fourth_point)
+		dice = Dice(first_point, second_point, third_point, fourth_point)
 		dice.draw_dice()
 		quantity_of_circles(first_point, second_point, third_point, fourth_point, center_point, dice_points)
 	points = calc_zonk_combo(dice_points)
@@ -100,9 +101,8 @@ def create_dice():
 	label_points.place(x=200, y=250)
 
 
-def quit():
+def exit_program():
 	exit()
-
 
 root = Tk()
 root.geometry('500x360')
@@ -111,7 +111,7 @@ place_dice = Canvas(root, width=400, height=200, bg='green')
 place_dice.place(x=10, y=10)
 
 button_play = Button(root, text="roll the dice", command=create_dice)
-button_quit = Button(root, text="Quit", command=quit)
+button_quit = Button(root, text="Quit", command=exit_program)
 
 button_play.place(x=100, y=300)
 button_quit.place(x=300, y=300)
