6f004de67f4cde7b3a3b26c510aaabeaff9a1c33
Mintimir <kachaev.666@gmail.com>
2020-04-10 20:17:33 +0600

    dragon_curve.py by OOP

--- lab3/dragon_curve.py
+++ lab3/dragon_curve.py
@@ -14,25 +14,31 @@ def distance(start: Point, end: Point):
 	return math.sqrt(dx ** 2 + dy ** 2)
 
 
-def draw_dragon_curve(start: Point, end: Point, draw: ImageDraw.ImageDraw):
-	length = distance(start, end)
-	if length < 1:
-		draw.line((int(round(start.x)), int(round(start.y)), int(round(end.x)), int(round(end.y))), fill=ImageColor.getrgb("red"))
-	else:
-		if (end.x - start.x) == 0:
-			line_angle = math.atan((start.y - end.y))
+class Dragon_curve:
+	def __init__(self, start: Point, end: Point):
+		self.start = start
+		self.end = end
+
+	def draw_dragon_curve(self, start: Point, end: Point, draw: ImageDraw.ImageDraw):
+		length = distance(start, end)
+		if length < 1:
+			draw.line((int(round(start.x)), int(round(start.y)), int(round(end.x)), int(round(end.y))),
+			          fill=ImageColor.getrgb("red"))
 		else:
-			line_angle = math.atan((start.y - end.y) / (end.x - start.x))
-			if end.x < start.x:
-				line_angle += math.pi
+			if (end.x - start.x) == 0:
+				line_angle = math.atan((start.y - end.y))
+			else:
+				line_angle = math.atan((start.y - end.y) / (end.x - start.x))
+				if end.x < start.x:
+					line_angle += math.pi
 
-		point3_angle = line_angle + math.pi / 4
-		point3_x = start.x + math.cos(point3_angle) * length / math.sqrt(2)
-		point3_y = start.y - math.sin(point3_angle) * length / math.sqrt(2)
-		point3 = Point(point3_x, point3_y)
+			point3_angle = line_angle + math.pi / 4
+			point3_x = start.x + math.cos(point3_angle) * length / math.sqrt(2)
+			point3_y = start.y - math.sin(point3_angle) * length / math.sqrt(2)
+			point3 = Point(point3_x, point3_y)
 
-		draw_dragon_curve(start, point3, draw)
-		draw_dragon_curve(end, point3, draw)
+			self.draw_dragon_curve(start, point3, draw)
+			self.draw_dragon_curve(end, point3, draw)
 
 
 width, height = 1200, 400
@@ -42,7 +48,8 @@ draw = ImageDraw.ImageDraw(image)
 
 start_point = Point(400, height - 200)
 end_point = Point(700, height - 200)
-draw_dragon_curve(start_point, end_point, draw)
+dragon = Dragon_curve(start_point, end_point)
+dragon.draw_dragon_curve(dragon.start, dragon.end, draw)
 
 image.save("dragon_curve.png")
 image.show()
